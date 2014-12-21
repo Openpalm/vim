e460f4fd9087e3d16e42e9359faa4ebddefcafb2
Shokodemon <Shokodemon@gmail.com>
2014-12-21 15:52:39 +0100

    fix script, ycm conf

---
+++ scripts/ycm_fix.sh
@@ -0,0 +1,10 @@
+sudo apt-get install build-essential cmake libclang-3.5-dev libboost-all-dev
+mkdir ~/.ycmtmp
+cd ~/.ycmtmp
+cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON  . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+make ycm_support_libs
+
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh --clang-completer
+
