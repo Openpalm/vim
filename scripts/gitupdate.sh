d76d7c035b3537e55cae3b4e8549a4cd4b588f03
Shokodemon <Shokodemon@gmail.com>
2015-02-03 20:03:16 +0100

    vimrc fix, gitupdate fix

--- scripts/gitupdate.sh
+++ scripts/gitupdate.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
- git config --global user.email "Shokodemon@gmail.com"
- git config --global user.name "Shokodemon"
+ git add . --all
 
- git add --all .
  git status
  git commit -a -m "$1"
  git push
