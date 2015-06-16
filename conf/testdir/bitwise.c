50e7ef6a972faebecde69af6f5f76f1792a6cfad
Shokodemon <Shokodemon@gmail.com>
2015-06-16 23:28:12 +0200

    neocompelte added

--- conf/testdir/bitwise.c
+++ conf/testdir/bitwise.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
 
 #define SIZE_UINT ((int) sizeof(unsigned int))
@@ -8,7 +8,6 @@
 
 unsigned int mkmask(int);
 void printBits(unsigned int);
-
 
 int issetBit(unsigned int, int);
 int setBit(unsigned int *, int);
@@ -20,114 +19,114 @@ int run(unsigned int *, int, int, int, int);
 
 int main(void)
 {
-	unsigned int a = 645;
-	unsigned int *testNum1 = &a;
-	run(testNum1, 1, 2, 1, 1);
+  unsigned int a = 645;
+  unsigned int *testNum1 = &a;
+  run(testNum1, 1, 2, 1, 1);
 
-	return 0;
+  return 0;
 }
 
 int run(unsigned int *n, int issetBitBit,
-		int setBitBit, int unsetBitBit, int toggleBitBit)
+    int setBitBit, int unsetBitBit, int toggleBitBit)
 {
 
-	printf("running sequence for the number: (%d)\n", *n);
-	printf("bit field: ");
-	printBits(*n);
+  printf("running sequence for the number: (%d)\n", *n);
+  printf("bit field: ");
+  printBits(*n);
 
-	SEP_LINE
+  SEP_LINE
 
-		printf("issetBit(%d)\n", issetBitBit);
-	printf("result:\t%s\n", (issetBit(*n, issetBitBit) == 0
-				? "no" : "yes"));
+    printf("issetBit(%d)\n", issetBitBit);
+  printf("result:\t%s\n", (issetBit(*n, issetBitBit) == 0
+        ? "no" : "yes"));
 
-	SEP_LINE
+  SEP_LINE
 
-		printf("setBit(%d)\n", setBitBit);
-	setBit(n, setBitBit);
-	printf("result\t");
-	printBits(*n);
+    printf("setBit(%d)\n", setBitBit);
+  setBit(n, setBitBit);
+  printf("result\t");
+  printBits(*n);
 
-	SEP_LINE
+  SEP_LINE
 
-		printf("unSetBit(%d)\n", unsetBitBit);
-	unsetBit(n, unsetBitBit);
-	printf("result\t");
-	printBits(*n);
+    printf("unSetBit(%d)\n", unsetBitBit);
+  unsetBit(n, unsetBitBit);
+  printf("result\t");
+  printBits(*n);
 
-	SEP_LINE
+  SEP_LINE
 
-		printf("toggleBit(%d)\n", toggleBitBit);
-	toggleBit(n, toggleBitBit);
-	printf("result\t");
-	printBits(*n);
+    printf("toggleBit(%d)\n", toggleBitBit);
+  toggleBit(n, toggleBitBit);
+  printf("result\t");
+  printBits(*n);
 
-	SEP_LINE
+  SEP_LINE
 
-		printf("setting right-most null bit\n");
-	rhsnullBit(n);
-	printf("result\t");
-	printBits(*n);
+    printf("setting right-most null bit\n");
+  rhsnullBit(n);
+  printf("result\t");
+  printBits(*n);
 
-	puts("");
+  puts("");
 
-	return 0;
+  return 0;
 }
 
 unsigned int mkmask(int n)
 {
-	unsigned int result = 1;
-	return result <<= (n-1);
+  unsigned int result = 1;
+  return result <<= (n-1);
 }
 
 void printBits(unsigned int num)
 {
-	unsigned int i = 0;
-	for (; i < SIZE_UINT*8; i++) {
-		if (((i % 4) == 0) && !(i == 0))
-			printf(" ");
-		printf("%u", num & MAX_2POWER ? 1 : 0);
-		num = num<<1;
-	}
-	puts("");
+  unsigned int i = 0;
+  for (; i < SIZE_UINT*8; i++) {
+    if (((i % 4) == 0) && !(i == 0))
+      printf(" ");
+    printf("%u", num & MAX_2POWER ? 1 : 0);
+    num = num<<1;
+  }
+  puts("");
 }
 
 
 int issetBit(unsigned int field, int n)
 {
-	unsigned int mask = mkmask(n);
-	return (field & mask) == mask ? 1 : 0;
+  unsigned int mask = mkmask(n);
+  return (field & mask) == mask ? 1 : 0;
 }
 
 
 int setBit(unsigned int *n , int bit)
 {
-	*n = (*n) | mkmask(bit);
-	return 0;
+  *n = (*n) | mkmask(bit);
+  return 0;
 }
 
 int unsetBit(unsigned int *n, int bit)
 {
-	*n = (*n) & (~mkmask(bit));
-	return 0;
+  *n = (*n) & (~mkmask(bit));
+  return 0;
 }
 
 int toggleBit(unsigned int *n, int bit)
 {
-	if (issetBit(*n, bit) == 0)
-		setBit(n, bit);
-	else
-		unsetBit(n, bit);
-	return 0;
+  if (issetBit(*n, bit) == 0)
+    setBit(n, bit);
+  else
+    unsetBit(n, bit);
+  return 0;
 }
 
 int rhsnullBit(unsigned int *n)
 {
-	unsigned int test = *n;
-	int highest_2power = 0;
-	for (; test > 0; ++highest_2power)
-		test = (unsigned int) test / 2;
-	setBit(n, highest_2power+1);
-	return 0;
+  unsigned int test = *n;
+  int highest_2power = 0;
+  for (; test > 0; ++highest_2power)
+    test = (unsigned int) test / 2;
+  setBit(n, highest_2power+1);
+  return 0;
 }
 
