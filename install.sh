a910aa71e4516e779388dbb1f582cfbc2b3228c7
Bassel Khatib <bassel.khatib@seitenbau.net>
2019-11-04 12:46:26 +0100

    retouched install sh

--- install.sh
+++ install.sh
@@ -4,11 +4,10 @@ git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 VRCPATH=~/.vimrc
 SRCPATH=~/.screenrc
 
-if test -f "$VRCPATH"; then mv ~/.vimrc ~/vimrc-legacy-"`date`";fi
-if test -f "$SRCPATH"; then mv ~/.screenrc ~/screen-legacy-"`date`";fi
+if test -f "$VRCPATH"; then mv ~/.vimrc ~/.vimrc-legacy-"`date +%F-%H:%M:%S`";fi
+if test -f "$SRCPATH"; then mv ~/.screenrc ~/.screen-legacy-"`date +%F-%H:%M:%S`";fi
 
 ln -s ~/.vim/scripts/rcs/vimrc ~/.vimrc
 ln -s ~/.vim/scripts/rcs/screenrc ~/.screenrc
 
 vim +PluginInstall +qall
-vim +PluginClean
