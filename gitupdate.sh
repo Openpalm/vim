f4b06702d3ff1e37ea97354000aaa734f73a245c
Bassel Khatib <basselkhatib@Bassels-MacBook-Pro.local>
2020-10-15 10:14:38 +0200

    generic push

--- gitupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
- git add . --all
-
- git status
- git commit -a -m "$1"
- git push
-
--- push.sh
+++ push.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
  git add . --all
-
  git status
- git commit -a -m "$1"
+ git commit -a -m "generic push"
  git push
 
