1cff382631f0aac9f9be44a049d438a1c57929d9
Bassel Khatib <basselkhatib@Bassels-MacBook-Pro.local>
2020-11-11 17:08:47 +0100

    pushing changes

--- push.sh
+++ push.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-today=`date`
 
-git add . --all
-git status
-
-sleep 1
-
-git commit -a -m "pushing changes $today"
-git push
+roday=`date` && git add . --all && git status sleep 1 && git commit -a -m "pushing changes $today" && git push
 
